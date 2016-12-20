adb6a7483bcd0907ee410ccf4b81f126d7bc5d3a
Haoran Li <lhrshitc@163.com>
2016-12-20 15:57:03 +0800

    train_rnn

--- train_rnn.py
+++ train_rnn.py
@@ -97,14 +97,14 @@ def define_simple_seg_rnn():
     print "define simple seg rnn"
     print "embedding_dim = %d" %embedding_dim
     print "hiden_dim = %d" %hiden_dim
-    w_reg = l2(0.0001)
-    b_reg = l2(0.0001)
+    w_reg = l2(0.0003)
+    b_reg = l2(0.0003)
     event_input = Input(shape = (max_segs, event_dim), name = "seg_event_input")
     masked = Masking(mask_value=0)(event_input)
-    emd = TimeDistributedDense(input_dim = event_dim, output_dim = embedding_dim , name = 'seg_event_embedding', init = "uniform",
-        bias = False)(masked)
-    lstm = LSTM(input_dim = embedding_dim, output_dim = hiden_dim, inner_activation='hard_sigmoid', activation='sigmoid',
-        W_regularizer = w_reg, b_regularizer = b_reg)(emd)
+    # emd = TimeDistributedDense(input_dim = event_dim, output_dim = embedding_dim , name = 'seg_event_embedding', init = "uniform",
+    #     bias = False)(masked)
+    # lstm = LSTM(input_dim = embedding_dim, output_dim = hiden_dim, inner_activation='hard_sigmoid', activation='sigmoid',
+    #     W_regularizer = w_reg, b_regularizer = b_reg)(emd)
     # lstm = LSTM(input_dim = event_dim, output_dim = hiden_dim, inner_activation='hard_sigmoid', activation='sigmoid',
         # W_regularizer = w_reg, b_regularizer = b_reg, input_length = None)(masked)
     pred = Dense(1, activation = "sigmoid", name = 'prediction')(lstm)
