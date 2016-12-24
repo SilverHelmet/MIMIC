464c374cf9c0ad8aeebb3b01a3b695ffae965ae8
Haoran Li <lhrshitc@163.com>
2016-12-25 00:28:12 +0800

    add max_seg_lenght.py

--- scripts/max_seg_length.py
+++ scripts/max_seg_length.py
@@ -6,11 +6,18 @@ if __name__ == "__main__":
     f = h5py.File(sys.argv[1], 'r')
     segs = f['segment'][:]
     max_seg_length = 0
+    seg_lengths = []
     for seg in segs:
         st = 0
         for ed in seg:
             if ed == 0:
                 break
             max_seg_length = max(max_seg_length, ed -st)
+            seg_lengths.append(ed - st)
             st = ed
     print max_seg_length
+    print 'size = %d' %(len(seg_lengths))
+    seg_lengths.sort(reversed = True)
+    print "up 100 = %d" %seg_lengths[100]
+    print "up 1000 = %d" %seg_lengths[1000]
+    print "up 10000 = %d" %seg_lengths[10000]
