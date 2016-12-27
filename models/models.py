1e3f513e063d958c90fabd2af75ed670cfb6bb92
Haoran Li <lhrshitc@163.com>
2016-12-27 22:24:11 +0800

    add event seq stat

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 from keras import backend as K
 from keras.layers.core import  Flatten, Lambda, Dense
 from keras.layers import merge
+from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import TimeDistributed
 
 
@@ -10,6 +11,90 @@ def SimpleAttentionRNN(rnn):
     alpha = MaskLambda(function = mask_softmax, name = 'alpha')(flatten_score)
     attention = merge([alpha, rnn], mode = 'dot', dot_axes = 1, name = 'attention')
     return attention
+
+class EventAttentionLSTM(LSTM):
+    
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(shape=input_shape)]
+        self.input_dim = input_shape[-1]
+
+        self.W = self.init((self.input_dim, 4 * self.output_dim),
+                            name='{}_W'.format(self.name))
+        self.U = self.inner_init((self.output_dim, 4 * self.output_dim),
+                                    name='{}_U'.format(self.name))
+        self.b = K.variable(np.hstack((np.zeros(self.output_dim),
+                                        K.get_value(self.forget_bias_init((self.output_dim,))),
+                                        np.zeros(self.output_dim),
+                                        np.zeros(self.output_dim))),
+                                name='{}_b'.format(self.name))
+        # event attention
+        self.Wea = self.init((self.input_dim, ),
+                            name = '{}_We'.format(self.name))
+        # output attention
+        self.Woa = self.inner_init((self.output_dim, ))
+
+        self.trainable_weights = [self.W, self.U, self.b, self.Wea, self.Wao]
+
+        self.regularizers = []
+        if self.W_regularizer:
+            self.W_regularizer.set_param(self.W)
+            self.regularizers.append(self.W_regularizer)
+        if self.U_regularizer:
+            self.U_regularizer.set_param(self.U)
+            self.regularizers.append(self.U_regularizer)
+        if self.b_regularizer:
+            self.b_regularizer.set_param(self.b)
+            self.regularizers.append(self.b_regularizer)
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+        self.built = True
+
+
+    def process(x, output):
+        '''
+            x: (samples, seg_length, input_dim)
+            output: (samples, output_dim)
+        '''
+        
+        mask = K.any(K.not_equal(x, 0.0), axis=-1)  # (samples, seg_length)
+        ea = K.dot(x, self.Wea)                     # (samples, seg_length) 
+        oa = K.dot(output, self.Woa)                # (samples,)
+        oa = K.expand_dims(oa, -1)                  # (samples, 1)
+        att = ea + oa                               # (samples, seg_length)
+        att = mask_softmax(att, x)                  # (samples. seg_length)
+        # att = K.tanh(ea + oa)                       # (samples, seg_length)
+        seg_emd = K.batch_dot(att, x)
+
+        return seg_emd
+
+    def step(self, x, states):
+        
+        
+        h_tm1 = states[0]
+        c_tm1 = states[1]
+        B_U = states[2]
+        B_W = states[3]
+        x = process(x, h_tm1)
+        
+        
+        # LSTM process
+        z = K.dot(x * B_W[0], self.W) + K.dot(h_tm1 * B_U[0], self.U) + self.b
+
+        z0 = z[:, :self.output_dim]
+        z1 = z[:, self.output_dim: 2 * self.output_dim]
+        z2 = z[:, 2 * self.output_dim: 3 * self.output_dim]
+        z3 = z[:, 3 * self.output_dim:]
+
+        i = self.inner_activation(z0)
+        f = self.inner_activation(z1)
+        c = f * c_tm1 + i * self.activation(z2)
+        o = self.inner_activation(z3)
+     
+
+        h = o * self.activation(c)
+        return h, [h, c]
 
     
 class MaskFlatten(Flatten):
