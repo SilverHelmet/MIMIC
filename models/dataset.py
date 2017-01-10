c23f2b89caac08e8d3a64742d2d785b5298bd2be
Haoran Li <lhrshitc@163.com>
2017-01-10 17:22:22 +0800

    fix bug

--- models/dataset.py
+++ models/dataset.py
@@ -131,6 +131,7 @@ def sample_generator(dataset, setting):
                     split_seg = segs[j]
                     seged_event.append(gen_seged_event_seq(events[j], split_seg, max_seg_length))
                 seged_event = np.array(seged_event)
+                print seged_event.shape
                 yield (seged_event, label)
             else:
                 # output shape (nb_sample, max_segs, event_dim)
