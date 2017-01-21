0b778aeae755dd87be8e49061b09c76912c2f018
Haoran Li <lhrshitc@163.com>
2017-01-21 14:39:08 +0800

    fix bug

--- shells/run_retain.sh
+++ shells/run_retain.sh
@@ -1,2 +1,2 @@
 dir="baseline/retainData/icu_catAtt."
-python baseline/retain.py --verbose "${dir}visit" 3418 "${dir}label" "${dir}weight" >& log/retain.log
+python -u  baseline/retain.py --verbose "${dir}visit" 3418 "${dir}label" "${dir}weight" >& log/retain.log
