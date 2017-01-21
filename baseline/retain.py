d93acda35a8caaaf18fe6feecf3e144a39584639
Haoran Li <lhrshitc@163.com>
2017-01-21 17:22:23 +0800

    fix bug

--- baseline/retain.py
+++ baseline/retain.py
@@ -369,7 +369,7 @@ def calculate_auc(test_model, dataset, options, calc_all = False):
         labels = dataset[1]
         ids = dataset[3]
         merged_labels = merge_label(labels, ids)
-        score = p.array(scoreVec)
+        score = np.array(scoreVec)
         merged_score = merge_prob(score, ids)
 
         auROC = roc_auc_score(labels, score)
