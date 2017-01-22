f093eb3a12707e434c104f21a6cfaf3e34503d5c
Haoran Li <lhrshitc@163.com>
2017-01-22 08:15:39 +0800

    add

--- shells/run_timeAggre_tAtt.sh
+++ shells/run_timeAggre_tAtt.sh
@@ -1,3 +1,6 @@
 python -u train_rnn.py settings/lstm.txt settings/temporal.txt>& log/icu_timeAggre_tAtt_lstm.log1 &
+wait
 python -u train_rnn.py settings/catAtt_lstm.txt settings/temporal.txt>& log/icu_timeAggre_catAtt_tAtt_lstm.log1 &
+wait
 python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/temporal.txt>& log/icu_timeAggre_fea_catAtt_tAtt_lstm.log1 &
+wait
