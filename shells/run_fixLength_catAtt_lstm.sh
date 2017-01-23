876df3cafb584aca35dd7ea8db06815928770676
Haoran Li <lhrshitc@163.com>
2017-01-24 00:48:02 +0800

    change nb_epoch to 15

--- shells/run_fixLength_catAtt_lstm.sh
+++ shells/run_fixLength_catAtt_lstm.sh
@@ -3,4 +3,3 @@ python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength8.txt >& log/
 python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength64.txt >& log/icu_fixLength64_catAtt_lstm.log1 
 python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength128.txt >& log/icu_fixLength128_catAtt_lstm.log1 
 python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength256.txt >& log/icu_fixLength256_catAtt_lstm.log1 
-python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength16.txt >& log/icu_fixLength16_catAtt_lstm.log1 
--- shells/run_fixLength_lstm.sh
+++ shells/run_fixLength_lstm.sh
@@ -2,5 +2,4 @@ python -u train_rnn.py settings/lstm.txt  settings/fixLength4.txt >& log/icu_fix
 python -u train_rnn.py settings/lstm.txt  settings/fixLength8.txt >& log/icu_fixLength8_lstm.log1 
 python -u train_rnn.py settings/lstm.txt  settings/fixLength64.txt >& log/icu_fixLength64_lstm.log1 
 python -u train_rnn.py settings/lstm.txt  settings/fixLength128.txt >& log/icu_fixLength128_lstm.log1 
-python -u train_rnn.py settings/lstm.txt  settings/fixLength256.txt >& log/icu_fixLength256_lstm.log1
-python -u train_rnn.py settings/lstm.txt  settings/fixLength16.txt >& log/icu_fixLength16_lstm.log1 
+python -u train_rnn.py settings/lstm.txt  settings/fixLength256.txt >& log/icu_fixLength256_lstm.log1 
