0c984bfb0d0e79e67e0fe2ebd677c48056cab860
Haoran Li <lhrshitc@163.com>
2017-01-24 00:50:44 +0800

    add settings

--- shells/run_fixLength_lstm.sh
+++ shells/run_fixLength_lstm.sh
@@ -1,5 +1,4 @@
-python -u train_rnn.py settings/lstm.txt  settings/fixLength4.txt >& log/icu_fixLength4_lstm.log1 
-python -u train_rnn.py settings/lstm.txt  settings/fixLength8.txt >& log/icu_fixLength8_lstm.log1 
-python -u train_rnn.py settings/lstm.txt  settings/fixLength64.txt >& log/icu_fixLength64_lstm.log1 
-python -u train_rnn.py settings/lstm.txt  settings/fixLength128.txt >& log/icu_fixLength128_lstm.log1 
-python -u train_rnn.py settings/lstm.txt  settings/fixLength256.txt >& log/icu_fixLength256_lstm.log1 
+python -u train_rnn.py settings/lstm.txt  settings/fixLength256.txt >& log/icu_fixLength256_lstm.log1
+python -u train_rnn.py settings/lstm.txt  settings/fixLength16.txt >& log/icu_fixLength16_lstm.log1 
+python -u train_rnn.py settings/lstm.txt  settings/fixLength2.txt >& log/icu_fixLength2_lstm.log1
+python -u train_rnn.py settings/lstm.txt  settings/fixLength1.txt >& log/icu_fixLength1_lstm.log1
