35bc08fdf774b25545f6c0ca3ba8a039a11aad3d
Haoran Li <lhrshitc@163.com>
2017-01-23 16:10:31 +0800

    add setting

---
+++ shells/run_fixLength_catAtt_lstm.sh
@@ -0,0 +1,5 @@
+python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength4.txt >& log/icu_fixLength4_catAtt_lstm.log1 
+python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength8.txt >& log/icu_fixLength8__catAtt_lstm.log1 
+python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength64.txt >& log/icu_fixLength64_catAtt_lstm.log1 
+python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength128.txt >& log/icu_fixLength128_catAtt_lstm.log1 
+python -u train_rnn.py settings/catAtt_lstm.txt  settings/fixLength256.txt >& log/icu_fixLength256_catAtt_lstm.log1 
