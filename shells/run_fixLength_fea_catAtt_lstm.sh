7c3a4dbeb82d7a634e7705429db4b8b10528649a
Haoran Li <lhrshitc@163.com>
2017-01-24 16:41:56 +0800

    add settings

--- shells/run_fixLength_fea_catAtt_lstm.sh
+++ shells/run_fixLength_fea_catAtt_lstm.sh
@@ -1,9 +1,11 @@
 dataset_setting="settings/fea_catAtt_lstm"
-python -u train_rnn.py $dataset_setting  settings/fixLength1.txt  >& log/icu_fixLength1_fea_catAtt_lstm.log1 
-python -u train_rnn.py $dataset_setting  settings/fixLength2.txt >& log/icu_fixLength2_catAtt_lstm.log1 
+
 python -u train_rnn.py $dataset_setting  settings/fixLength4.txt >& log/icu_fixLength4_fea_catAtt_lstm.log1 
-python -u train_rnn.py $dataset_setting  settings/fixLength8.txt >& log/icu_fixLength8_fea_catAtt_lstm.log1 
+# python -u train_rnn.py $dataset_setting  settings/fixLength8.txt >& log/icu_fixLength8_fea_catAtt_lstm.log1 
 python -u train_rnn.py $dataset_setting  settings/fixLength16.txt >& log/icu_fixLength16_fea_catAtt_lstm.log1 
-python -u train_rnn.py $dataset_setting  settings/fixLength64.txt >& log/icu_fixLength64_fea_catAtt_lstm.log1 
+# python -u train_rnn.py $dataset_setting  settings/fixLength64.txt >& log/icu_fixLength64_fea_catAtt_lstm.log1 
 python -u train_rnn.py $dataset_setting  settings/fixLength128.txt >& log/icu_fixLength128_fea_catAtt_lstm.log1 
-python -u train_rnn.py $dataset_setting  settings/fixLength256.txt >& log/icu_fixLength256_fea_catAtt_lstm.log1 
+# python -u train_rnn.py $dataset_setting  settings/fixLength256.txt >& log/icu_fixLength256_fea_catAtt_lstm.log1 
+
+# python -u train_rnn.py $dataset_setting  settings/fixLength1.txt  >& log/icu_fixLength1_fea_catAtt_lstm.log1 
+# python -u train_rnn.py $dataset_setting  settings/fixLength2.txt >& log/icu_fixLength2_catAtt_lstm.log1 
