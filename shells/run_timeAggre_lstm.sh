fc2853f0ad32e163614a71f2b4b5eb9d80ff82d2
Haoran Li <lhrshitc@163.com>
2017-01-26 21:39:30 +0800

    change to death

--- shells/run_timeAggre_lstm.sh
+++ shells/run_timeAggre_lstm.sh
@@ -1,6 +1,12 @@
-python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt >& log/death_timeAggre_lstm.log1 &
-wait
+# python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt >& log/death_timeAggre_lstm.log1 &
+# wait
 python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt >& log/death_timeAggre_catAtt_lstm.log1 &
 wait
 python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt >& log/death_timeAggre_fea_catAtt_lstm.log1 &
+wait
+python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/event_temporal.txt>& log/death_timeAggre_eAtt_tAtt_lstm.log1 &
+wait
+python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/event_temporal.txt >& log/death_timeAggre_catAtt_eAtt_tAtt_lstm.log1 &
+wait
+python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt settings/event_temporal.txt >& log/death_timeAggre_fea_catAtt_eAtt_tAtt_lstm.log1 &
 wait
