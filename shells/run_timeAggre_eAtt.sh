cfa1cf9169bc9bfafa2e4979593f72fadfcd60b5
Haoran Li <lhrshitc@163.com>
2017-01-26 17:58:56 +0800

    change to death

--- shells/run_timeAggre_eAtt.sh
+++ shells/run_timeAggre_eAtt.sh
@@ -1,6 +1,6 @@
-python -u train_rnn.py settings/lstm.txt settings/event.txt>& log/icu_timeAggre_eAtt_lstm.log1 &
+python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/event.txt>& log/icu_timeAggre_eAtt_lstm.log1 &
 wait
-python -u train_rnn.py settings/catAtt_lstm.txt settings/event.txt>& log/icu_timeAggre_catAtt_eAtt_lstm.log1 &
+python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt  settings/event.txt>& log/icu_timeAggre_catAtt_eAtt_lstm.log1 &
 wait
-python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/event.txt>& log/icu_timeAggre_fea_catAtt_eAtt_lstm.log1 &
+python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt  settings/event.txt>& log/icu_timeAggre_fea_catAtt_eAtt_lstm.log1 &
 wait
