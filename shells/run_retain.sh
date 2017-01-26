36a590b71d3e16f628085c97684d77e065170106
Haoran Li <lhrshitc@163.com>
2017-01-26 11:25:06 +0800

    change to death

--- shells/run_retain.sh
+++ shells/run_retain.sh
@@ -1,6 +1,13 @@
-dir="baseline/retainData/icu_catAtt."
-python -u  baseline/retain.py --verbose "${dir}visit" 3418 "${dir}label" "${dir}weight" >& log/retain_icu_catAtt.log &
+# dir="baseline/retainData/icu_catAtt."
+# python -u  baseline/retain.py --verbose "${dir}visit" 3418 "${dir}label" "${dir}weight" >& log/retain_icu_catAtt.log &
 
-dir="baseline/retainData/icu."
-python -u  baseline/retain.py --verbose "${dir}visit" 951 "${dir}label" "${dir}weight" >& log/retain_icu.log &
+# dir="baseline/retainData/icu."
+# python -u  baseline/retain.py --verbose "${dir}visit" 951 "${dir}label" "${dir}weight" >& log/retain_icu.log &
+# wait
+
+dir="baseline/retainData/death_catAtt."
+python -u  baseline/retain.py --verbose "${dir}visit" 3418 "${dir}label" "${dir}weight" >& log/retain_death_catAtt.log &
+
+dir="baseline/retainData/death."
+python -u  baseline/retain.py --verbose "${dir}visit" 951 "${dir}label" "${dir}weight" >& log/retain_death.log &
 wait
