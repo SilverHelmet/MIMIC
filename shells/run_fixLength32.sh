b851fcc37ebc1f99f5e23b8131cb333e6c45c5d5
Haoran Li <lhrshitc@163.com>
2017-01-27 22:15:08 +0800

    change to death

--- shells/run_fixLength32.sh
+++ shells/run_fixLength32.sh
@@ -1,12 +1,12 @@
 # python -u train_rnn.py settings/lstm.txt settings/fixLength32.txt >& log/death_fixLength32_lstm.log1 &
 # wait
-python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength32.txt >& log/death_fixLength32_catAtt_lstm.log1 &
-wait
-python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt >& log/death_fixLength32_fea_catAtt_lstm.log1 &
-wait
+# python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength32.txt >& log/death_fixLength32_catAtt_lstm.log1 &
+# wait
+# python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt >& log/death_fixLength32_fea_catAtt_lstm.log1 &
+# wait
 python -u train_rnn.py settings/lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/death_fixLength32_eAtt_tAtt_lstm.log1 &
 wait
 python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/death_fixLength32_catAtt_eAtt_tAtt_lstm.log1 &
 wait
-python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/death_fixLength32_fea_catAtt_eAtt_tAtt_lstm.log1 &
-wait
+# python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/death_fixLength32_fea_catAtt_eAtt_tAtt_lstm.log1 &
+# wait
