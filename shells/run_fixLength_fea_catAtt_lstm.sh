7d91e496f44f3be0688001d8a88a70bc20d10f29
Haoran Li <lhrshitc@163.com>
2017-02-01 09:37:11 +0800

    change to icu

--- shells/run_fixLength_fea_catAtt_lstm.sh
+++ shells/run_fixLength_fea_catAtt_lstm.sh
@@ -7,5 +7,5 @@ dataset_setting="settings/fea_catAtt_lstm.txt"
 # python -u train_rnn.py $dataset_setting  settings/fixLength128.txt >& log/death_fixLength128_fea_catAtt_lstm.log1 
 # python -u train_rnn.py $dataset_setting  settings/fixLength256.txt >& log/death_fixLength256_fea_catAtt_lstm.log1 
 
-python -u train_rnn.py $dataset_setting  settings/fixLength1.txt  >& log/death_fixLength1_fea_catAtt_lstm.log1 
-python -u train_rnn.py $dataset_setting  settings/fixLength2.txt >& log/death_fixLength2_fea_catAtt_lstm.log1 
+python -u train_rnn.py $dataset_setting  settings/fixLength1.txt  >& log/icu_fixLength1_fea_catAtt_lstm.log1 
+python -u train_rnn.py $dataset_setting  settings/fixLength2.txt >& log/icu_fixLength2_fea_catAtt_lstm.log1 
