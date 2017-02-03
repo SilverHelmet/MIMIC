9e9c3e602486a5a95d3b753068b884d6b4f05de9
Haoran Li <lhrshitc@163.com>
2017-02-03 12:42:46 +0800

    norm feature

--- shells/norm_feature.sh
+++ shells/norm_feature.sh
@@ -1,21 +1,21 @@
-python scripts/norm_feature.py \
+python -u scripts/norm_feature.py \
     ICU_exper/ICUIn_train_1000.h5 \
     ICU_exper/ICUIn_valid_1000.h5 \
     ICU_exper/ICUIn_test_1000.h5 >& log/norm_feature_icu.log &
 
-python scripts/norm_feature.py \
+python -u scripts/norm_feature.py \
     ICU_merged_exper/ICUIn_train_1000.h5 \
     ICU_merged_exper/ICUIn_valid_1000.h5 \
     ICU_merged_exper/ICUIn_test_1000.h5 >& log/norm_feature_icuMerged.log &
 
 wait
 
-python scripts/norm_feature.py \
+python -u scripts/norm_feature.py \
     death_exper/death_train_1000.h5 \
     death_exper/death_valid_1000.h5 \
     death_exper/death_test_1000.h5 >& log/norm_feature_death.log &
 
-python scripts/norm_feature.py \
+python -u scripts/norm_feature.py \
     death_merged_exper/death_train_1000.h5 \
     death_merged_exper/death_valid_1000.h5 \
     death_merged_exper/death_test_1000.h5 >& log/norm_feature_deathMerged.log &
