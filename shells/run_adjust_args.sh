7208d110fa809f8cb2cd29b32c48e6474c503741
Haoran Li <lhrshitc@163.com>
2017-02-03 13:48:14 +0800

    add norm feature api

--- shells/run_adjust_args.sh
+++ shells/run_adjust_args.sh
@@ -3,16 +3,18 @@ hidden_dim=64
 
 embedding_args="32 64 128"
 hidden_args="32 64 128"
+att_hidden_args="32 64"
 settings="settings/fea_catAtt_lstm.txt settings/timeAggre.txt  settings/event_temporal.txt"
 for embedding_dim in $embedding_args
 do
     for hidden_dim in $hidden_args
     do
-        args="embedding_dim=${embedding_dim}|hidden_dim=${hidden_dim}"
-        if [ "$1" = "norm" ]; then
-            args="$args|norm_feature=True"
-        fi
-        outfile="log/icu_timeAggre_attention_$args.log"
-        python -u train_rnn.py $settings "#$args" >& $outfile 
+        for att_hidden_dim in $att_hidden_args
+            args="embedding_dim=${embedding_dim}|hidden_dim=${hidden_dim}|att_hidden_dim=${att_hidden_dim}"
+            if [ "$1" = "norm" ]; then
+                args="$args|norm_feature=True"
+            fi
+            outfile="log/icu_timeAggre_attention_$args.log"
+            python -u train_rnn.py $settings "#$args" >& $outfile 
     done
 done
