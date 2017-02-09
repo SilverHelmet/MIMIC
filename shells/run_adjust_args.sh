111a7e0898b91c96e25565813d787b8eac067766
Haoran Li <lhrshitc@163.com>
2017-02-09 16:54:34 +0800

    add gather statistics

--- shells/run_adjust_args.sh
+++ shells/run_adjust_args.sh
@@ -1,10 +1,10 @@
 embedding_dim=64
 hidden_dim=64
 
-embedding_args="32 64 128"
+embedding_args="64"
 hidden_args="32 64 128"
 att_hidden_args="64 32 128"
-batch_args="32 64 128"
+batch_args="32"
 settings="settings/fea_catAtt_lstm.txt settings/timeAggre.txt  settings/event_temporal.txt"
 for embedding_dim in $embedding_args
 do
