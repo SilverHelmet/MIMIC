e9d7e45a99cf89270f4dba1621111bad19ff7444
Haoran Li <lhrshitc@163.com>
2017-02-14 09:53:48 +0800

    c

--- shells/run_timeAggre_lstm.sh
+++ shells/run_timeAggre_lstm.sh
@@ -1,20 +1,22 @@
-python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/event_temporal.txt>& log/death_timeAggre_eAtt_tAtt_lstm.log1 &
+prefix="THEANO_FLAGS=device=gpu2,floatX=float32"
+$prefix python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/event_temporal.txt>& log/icu_timeAggre_eAtt_tAtt_lstm.log2 &
 wait
-python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/event_temporal.txt >& log/death_timeAggre_catAtt_eAtt_tAtt_lstm.log1 &
+$prefix python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/event_temporal.txt >& log/icu_timeAggre_catAtt_eAtt_tAtt_lstm.log2 &
 wait
 
 # event attention
-python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/event.txt>& log/death_timeAggre_eAtt_lstm.log1 &
+$prefix python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/event.txt>& log/icu_timeAggre_eAtt_lstm.log2 &
 wait
-python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/event.txt >& log/death_timeAggre_catAtt_eAtt_lstm.log1 &
+$prefix python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/event.txt >& log/icu_timeAggre_catAtt_eAtt_lstm.log2 &
 wait
-python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt settings/event.txt >& log/death_timeAggre_fea_catAtt_eAtt_lstm.log1 &
+$prefix python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt settings/event.txt >& log/icu_timeAggre_fea_catAtt_eAtt_lstm.log2 &
 wait
 
 # temporal attention
-python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/temporal.txt>& log/death_timeAggre_tAtt_lstm.log1 &
+$prefix python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/temporal.txt>& log/icu_timeAggre_tAtt_lstm.log2 &
 wait
-python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/temporal.txt >& log/death_timeAggre_catAtt_tAtt_lstm.log1 &
+$prefix python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/temporal.txt >& log/icu_timeAggre_catAtt_tAtt_lstm.log2 &
 wait
-python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt settings/temporal.txt >& log/death_timeAggre_fea_catAtt_tAtt_lstm.log1 &
+$prefix python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt settings/temporal.txt >& log/icu_timeAggre_fea_catAtt_tAtt_lstm.log2 &
 wait
+
