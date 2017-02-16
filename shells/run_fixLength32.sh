c79112f7ca1202635248d0dc0fb02c0b2ea88f83
Haoran Li <lhrshitc@163.com>
2017-02-16 21:40:04 +0800

    c

--- shells/run_fixLength32.sh
+++ shells/run_fixLength32.sh
@@ -1,12 +1,12 @@
-# python -u train_rnn.py settings/lstm.txt settings/fixLength32.txt >& log/icu_fixLength32_lstm.log2 &
+# python -u train_rnn.py settings/lstm.txt settings/fixLength32.txt >& log/death_fixLength32_lstm.log2 &
 # wait
-# python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength32.txt >& log/icu_fixLength32_catAtt_lstm.log2 &
+# python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength32.txt >& log/death_fixLength32_catAtt_lstm.log2 &
 # wait
-# python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt >& log/icu_fixLength32_fea_catAtt_lstm.log2 &
+# python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt >& log/death_fixLength32_fea_catAtt_lstm.log2 &
 # wait
-python -u train_rnn.py settings/lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/icu_fixLength32_eAtt_tAtt_lstm.log2 &
+python -u train_rnn.py settings/lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/death_fixLength32_eAtt_tAtt_lstm.log2 &
 wait
-python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/icu_fixLength32_catAtt_eAtt_tAtt_lstm.log2 &
+python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/death_fixLength32_catAtt_eAtt_tAtt_lstm.log2 &
 wait
-python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/icu_fixLength32_fea_catAtt_eAtt_tAtt_lstm.log2 &
+python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength32.txt settings/event_temporal.txt >& log/death_fixLength32_fea_catAtt_eAtt_tAtt_lstm.log2 &
 wait
