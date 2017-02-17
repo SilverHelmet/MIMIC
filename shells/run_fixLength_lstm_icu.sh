a1ff92cac69836f31a9b7f9e2ad94f006078cec3
Haoran Li <lhrshitc@163.com>
2017-02-17 20:30:21 +0800

    c

--- shells/run_fixLength_lstm_icu.sh
+++ shells/run_fixLength_lstm_icu.sh
@@ -1,10 +1,10 @@
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength256.txt >& log/icu_fixLength256_lstm.log2
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength128.txt >& log/icu_fixLength128_lstm.log2
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength64.txt >& log/icu_fixLength64_lstm.log2
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength32.txt >& log/icu_fixLength32_lstm.log2 
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength16.txt >& log/icu_fixLength16_lstm.log2 
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength8.txt >& log/icu_fixLength8_lstm.log2 
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength4.txt >& log/icu_fixLength4_lstm.log2 
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength2.txt >& log/icu_fixLength2_lstm.log2
-THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength1.txt >& log/icu_fixLength1_lstm.log2
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength256_icu.txt >& log/icu_fixLength256_lstm.log2
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength128_icu.txt >& log/icu_fixLength128_lstm.log2
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength64_icu.txt >& log/icu_fixLength64_lstm.log2
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength32_icu.txt >& log/icu_fixLength32_lstm.log2 
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength16_icu.txt >& log/icu_fixLength16_lstm.log2 
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength8_icu.txt >& log/icu_fixLength8_lstm.log2 
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength4_icu.txt >& log/icu_fixLength4_lstm.log2 
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength2_icu.txt >& log/icu_fixLength2_lstm.log2
+THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength1_icu.txt >& log/icu_fixLength1_lstm.log2
 
