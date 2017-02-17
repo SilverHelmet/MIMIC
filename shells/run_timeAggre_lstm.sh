e6d7d8c2c4fee9db94d02fef1312920b10c91856
Haoran Li <lhrshitc@163.com>
2017-02-17 20:41:10 +0800

    c

--- shells/run_timeAggre_lstm.sh
+++ shells/run_timeAggre_lstm.sh
@@ -1,3 +1,4 @@
+# event & temporal attention
 prefix="THEANO_FLAGS=device=gpu2,floatX=float32"
 python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/event_temporal.txt>& log/death_timeAggre_eAtt_tAtt_lstm.log2
 
@@ -18,5 +19,12 @@ python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  settings/tempor
 python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt settings/temporal.txt >& log/death_timeAggre_catAtt_tAtt_lstm.log2
 
 python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt settings/temporal.txt >& log/deathtimeAggre_fea_catAtt_tAtt_lstm.log2
+
+# no attention
+python -u train_rnn.py settings/lstm.txt settings/timeAggre.txt  >& log/death_timeAggre_lstm.log2
+
+python -u train_rnn.py settings/catAtt_lstm.txt settings/timeAggre.txt >& log/death_timeAggre_catAtt_lstm.log2
+
+python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/timeAggre.txt  >& log/death_timeAggre_fea_catAtt_lstm.log2
 
 
