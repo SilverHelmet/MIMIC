e36e6602e99303e13dbaf55fe27a16bc7b932e4f
Haoran Li <lhrshitc@163.com>
2017-02-19 20:11:36 +0800

    c

--- shells/run_death.sh
+++ shells/run_death.sh
@@ -2,7 +2,6 @@ THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm.txt
 THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength1.txt >& log/death_catAtt_fixLength1_lstm.log2 &
 wait
 
-THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength1.txt  >& log/death_fea_catAtt_fixLength1_lstm.log2
 
 THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength2.txt >& log/death_fixLength2_lstm.log2 &
 python -u train_rnn.py settings/cat_lstm.txt settings/fixLength2.txt >& log/death_catAtt_fixLength2_lstm.log2 &
