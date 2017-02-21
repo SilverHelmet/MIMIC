eb3a94d3a33da4f19e6a5da60cf0392932408ec8
Haoran Li <lhrshitc@163.com>
2017-02-21 14:16:44 +0800

    c

--- shells/run_death.sh
+++ shells/run_death.sh
@@ -1,11 +1,7 @@
-THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength1.txt >& log/death_fixLength1_lstm.log2 &
-THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength1.txt >& log/death_catAtt_fixLength1_lstm.log2 &
-wait
 
-# THEANO_FLAGS=device=gpu1,floatX=float32 python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength1.txt  >& log/death_fea_catAtt_fixLength1_lstm.log2
+python -u train_rnn.py settings/lstm.txt  settings/fixLength2.txt >& log/death_fixLength2_lstm.log2 
+python -u train_rnn.py settings/cat_lstm.txt settings/fixLength2.txt >& log/death_fixLength2_catAtt_lstm.log2 
+python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength2.txt  >& log/death_fixLength2_fea_catAtt_lstm.log2
 
-# THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm.txt  settings/fixLength2.txt >& log/death_fixLength2_lstm.log2 &
-# python -u train_rnn.py settings/cat_lstm.txt settings/fixLength2.txt >& log/death_catAtt_fixLength2_lstm.log2 &
-# wait
-
-# THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/fea_catAtt_lstm.txt settings/fixLength2.txt  >& log/death_fea_catAtt_fixLength2_lstm.log2
+python -u train_rnn.py settings/fea_catAtt_lstm_icu.txt settings/fixLength2_icu.txt  >& log/icu_fixLength2_fea_catAtt_lstm.log1
+python -u train_rnn.py settings/fea_catAtt_lstm_icu.txt settings/fixLength2_icu.txt  >& log/icu_fixLength2_fea_catAtt_lstm.log2
