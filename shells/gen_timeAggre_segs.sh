e034780cf6d0c92744d25e39fc340573f79fe2a0
Haoran Li <lhrshitc@163.com>
2017-03-01 11:10:48 +0800

    v1

--- shells/gen_timeAggre_segs.sh
+++ shells/gen_timeAggre_segs.sh
@@ -1,9 +1,12 @@
 # dir=ICU_exper
 # files="ICUIn_train_1000.h5 ICUIn_valid_1000.h5 ICUIn_test_1000.h5"
 # seg_dir=ICU_exper/segs
-dir=death_exper
-files="death_test_1000.h5 death_train_1000.h5 death_valid_1000.h5"
-seg_dir=death_exper/segs
+# dir=death_exper
+# files="death_test_1000.h5 death_train_1000.h5 death_valid_1000.h5"
+# seg_dir=death_exper/segs
+dir=zhu_data
+files="train.h5 valid.h5 test.h5"
+seg_dir=zhu_data/segs
 
 for file in $files
 do
