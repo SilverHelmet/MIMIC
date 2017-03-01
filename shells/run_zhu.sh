87f2dffe47ef7874a963fed86ee836e767de1247
Haoran Li <lhrshitc@163.com>
2017-03-01 14:23:05 +0800

    v1

--- shells/run_zhu.sh
+++ shells/run_zhu.sh
@@ -1,4 +1,4 @@
-python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/temporal.txt>& log/zhu_timeAggre_tAtt_lstm.log1
-python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event.txt>& log/zhu_timeAggre_eAtt_lstm.log1
-python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event_temporal.txt>& log/zhu_timeAggre_eAtt_tAtt_lstm.log1
-python -u train_rnn.py settings/lstm_zhu.txt settings/fixLength32_zhu.txt  settings/event_temporal.txt>& log/zhu_fixLenght32_eAtt_tAtt_lstm.log1
+THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/temporal.txt>& log/zhu_timeAggre_tAtt_lstm.log1
+THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event.txt>& log/zhu_timeAggre_eAtt_lstm.log1
+THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event_temporal.txt>& log/zhu_timeAggre_eAtt_tAtt_lstm.log1
+THEANO_FLAGS=device=gpu0,floatX=float32 python -u train_rnn.py settings/lstm_zhu.txt settings/fixLength32_zhu.txt  settings/event_temporal.txt>& log/zhu_fixLenght32_eAtt_tAtt_lstm.log1
