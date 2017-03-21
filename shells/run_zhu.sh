ada13d11dad9bae533cad6f214e83a2d3ad9a6b9
Haoran Li <lhrshitc@163.com>
2017-03-21 17:32:37 +0800

    init

--- shells/run_zhu.sh
+++ shells/run_zhu.sh
@@ -1,4 +1,4 @@
-python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event.txt>& log/zhu_timeAggre_eAtt_lstm.log1
-python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/temporal.txt>& log/zhu_timeAggre_tAtt_lstm.log1
-python -u train_rnn.py settings/lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event_temporal.txt>& log/zhu_timeAggre_eAtt_tAtt_lstm.log1
-python -u train_rnn.py settings/lstm_zhu.txt settings/fixLength32_zhu.txt  settings/event_temporal.txt>& log/zhu_fixLenght32_eAtt_tAtt_lstm.log1
+python -u train_rnn.py settings/catAtt_lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event.txt>& log/zhu_timeAggre_eAtt_catAtt_lstm.log1
+python -u train_rnn.py settings/catAtt_lstm_zhu.txt settings/timeAggre_zhu.txt  settings/temporal.txt>& log/zhu_timeAggre_tAtt_catAtt_lstm.log1
+python -u train_rnn.py settings/catAtt_lstm_zhu.txt settings/timeAggre_zhu.txt  settings/event_temporal.txt>& log/zhu_timeAggre_eAtt_tAtt_catAtt_lstm.log1
+python -u train_rnn.py settings/catAtt_lstm_zhu.txt settings/fixLength32_zhu.txt  settings/event_temporal.txt>& log/zhu_fixLenght32_eAtt_tAtt_catAtt_lstm.log1
