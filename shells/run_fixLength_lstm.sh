de693a2c12d95a4e65239e3570c796cb3579fb97
Haoran Li <lhrshitc@163.com>
2017-03-22 09:50:46 +0800

    init

--- shells/run_fixLength_lstm.sh
+++ shells/run_fixLength_lstm.sh
@@ -9,3 +9,15 @@ python -u train_rnn.py settings/catAtt_lstm_zhu.txt  settings/noatt.txt settings
 python -u train_rnn.py settings/catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength2_zhu.txt >& log/zhu_fixLength2_catAtt_lstm.log1
 python -u train_rnn.py settings/catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength1_zhu.txt >& log/zhu_fixLength1_catAtt_lstm.log1
 
+
+
+
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength256_zhu.txt >& log/zhu_fixLength256_fea_catAtt_lstm.log1
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength128_zhu.txt >& log/zhu_fixLength128_fea_catAtt_lstm.log1
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength64_zhu.txt >& log/zhu_fixLength64_fea_catAtt_lstm.log1
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength32_zhu.txt >& log/zhu_fixLength32_fea_catAtt_lstm.log1 
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength16_zhu.txt >& log/zhu_fixLength16_fea_catAtt_lstm.log1 
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength8_zhu.txt >& log/zhu_fixLength8_fea_catAtt_lstm.log1 
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength4_zhu.txt >& log/zhu_fixLength4_fea_catAtt_lstm.log1 
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength2_zhu.txt >& log/zhu_fixLength2_fea_catAtt_lstm.log1
+python -u train_rnn.py settings/fea_catAtt_lstm_zhu.txt  settings/noatt.txt settings/fixLength1_zhu.txt >& log/zhu_fixLength1_fea_catAtt_lstm.log1
