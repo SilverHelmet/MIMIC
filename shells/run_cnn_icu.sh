4e0814bd2e35b5f7fa2b7444fdcf654732225211
Haoran Li <lhrshitc@163.com>
2017-03-23 22:44:56 +0800

    init

--- shells/run_cnn_icu.sh
+++ shells/run_cnn_icu.sh
@@ -1,2 +1,3 @@
 THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/lstm_icu.txt  settings/fixLength32_icu.txt settings/cnn.txt >& log/icu_fixLength32_cnn.log1 &
 THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/catAtt_lstm_icu.txt  settings/fixLength32_icu.txt settings/cnn.txt >& log/icu_fixLength32_catAtt_cnn.log1 &
+wait
