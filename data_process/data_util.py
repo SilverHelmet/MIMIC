d03b60bc25d69f3dde239485f963d5fb640adb92
Haoran Li <lhrshitc@163.com>
2018-03-27 14:14:37 +0800

    auto update

--- data_process/data_util.py
+++ data_process/data_util.py
@@ -11,19 +11,20 @@ def load_feature_map(filepath = None):
     for line in file(filepath):
         key, idx = line.split('\t')
         feature_map[key] = int(idx)
-        max_idx = max(max_idx, idx)
+        max_idx = max(max_idx, int(idx))
     return feature_map, max_idx + 1
 
 def load_sample_info(filepath):
     sample_info_map = {}
     for line in file(filepath):
         obj = json.loads(line)
+        pid = obj['pid']
         settings = obj['settings']
         for setting in settings:
             sid = setting['sample_id']
             hid = setting['hid']
             st = parse_time(setting['st'])
-            sample_info_map[sid] = {'hid': hid, 'st': st}
+            sample_info_map[sid] = {'pid': pid, hid': hid, 'st': st}
     return sample_info_map
 
 
--- data_process/gen_static_feature.py
+++ data_process/gen_static_feature.py
@@ -1,15 +1,13 @@
 import os
-from util import Print, event_seq_stat_dir, static_data_dir
+from util import Print, event_seq_stat_dir, static_data_dir, parse_time
 from .data_util import load_feature_map, load_sample_info
 import glob
-
+import json
 
 class StaticFeature:
-    
-    def __init__(self, feature_map, base_dir = None):
-        if base_dir is None:
-            base_dir = static_data_dir
+    def __init__(self, feature_map, feature_size, base_dir):
         self.feature_map = feature_map
+        self.feature_size = feature_size
         self.base_dir = base_dir
         self.feature_name_set = self.get_feature_name_set(self.feature_map)
         self.p_info_map = {}
@@ -27,7 +25,8 @@ class StaticFeature:
     def load(self):
         self.load_static_info_from_file(os.path.join(self.base_dir, 'admissions.json'), 'hadm_id', self.h_info_map)
         self.load_static_info_from_file(os.path.join(self.base_dir, 'diagnoses_icd.json'), 'hadm_id', self.h_info_map)
-        self.load_static_info_from_file(os.path.join(self.base_dir, 'diagnoses_icd.json'), 'hadm_id', self.h_info_map)
+        self.load_static_info_from_file(os.path.join(self.base_dir, 'patients.json'), 'subject_id', self.p_info_map)
+        self.load_static_info_from_file(os.path.join(self.base_dir, 'procedures_icd.json'), 'hadm_id', self.h_info_map)
 
     def load_static_info_from_file(self, filepath, key_name, info_map):
         table = os.path.basename(filepath).split('.')[0]
@@ -41,17 +40,28 @@ class StaticFeature:
             for name in obj:
                 if not name in self.feature_name_set:
                     continue
-                info[table + "#" + name] = obj[name]
+                if obj[name] != ""
+                    info[table + "#" + name] = obj[name]
 
+    def get_static_feature(self, info, vec, sample_info):
+
+        for name in info:
+            value = info[value]
+            if name == 'patients#dob':
+                idx = self.feature_map[name]
+                x = sample_info['st'] - 
+            else:
+                idx = self.feature_map[name + "#" + value]
+                x = 1 
             
-                
+            
+            feature = name + "#" + value
 
-
-    def load_admission(self):
-        path = os.path.join(self.base_dir, 'admissions.json')
-
-
-
+    def gen_feature_of_sample(self, sample_info):
+        vec = [0] * self.feature_size
+        self.get_static_feature(self.p_info_map[pid], vec, sample_info)
+        self.get_static_feature(self.h_info_map[hid], vec, sample_info)
+        return vec
 
 if __name__ == "__main__":
     death_sample_setting_path = os.path.join(event_seq_stat_dir, 'death_sample_setting.txt')
@@ -59,7 +69,18 @@ if __name__ == "__main__":
 
     sample_setting_path = death_sample_setting_path
 
-    feature_map = load_feature_map()
+    feature_map, n_features = load_feature_map()
     sample_info_map = load_sample_info(sample_setting_path)
 
-    static_info_map = load_static_info()
+    static_feature = StaticFeature(feature_map, n_features, os.path.join(static_data_dir, 'static_feature'))
+    static_feature.load()
+
+    sample_info = {
+        'pid': 2,
+        'hid': 163353,
+        'st': parse_time('2138-07-17 19:04:00')
+    }
+
+    static_feature.gen_feature_of_sample(sample_info)
+
+
