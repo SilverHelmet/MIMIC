ee1ea28c00ff97c4d5fa613d6139c19eb1e0c023
Haoran Li <lhrshitc@163.com>
2018-05-09 12:12:31 +0800

    auto update

--- shells/run_death.sh
+++ shells/run_death.sh
@@ -3,7 +3,7 @@
 THEANO_FLAGS=device=gpu1,floatX=float32 python -u train_rnn.py settings/gcn.txt settings/catAtt_lstm.txt settings/fixLength16.txt settings/params/lr=0.03.txt >& log/death_fixLength16_catAtt_gcn_lr0.03.log1 &
 THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/gcn.txt settings/catAtt_lstm.txt settings/fixLength16.txt settings/params/lr=0.003.txt >& log/death_fixLength16_catAtt_gcn_lr0.003.log2 &
 # THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/catAtt_lstm.txt settings/dlstm.txt settings/static_feature.txt settings/fixLength16.txt >& log/death_catAtt_dlstm.log1 &
-wait
+# wait
 # THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/gcn.txt settings/catAtt_lstm.txt settings/fixLength16.txt settings/static_feature.txt >& log/death_fixLength16_catAtt_staticfea_gcn.log6 &
 # wait
 # THEANO_FLAGS=device=gpu2,floatX=float32 python -u train_rnn.py settings/catAtt_lstm.txt settings/fixLength16.txt settings/static_feature.txt >& log/death_fixLength16_catAtt_staticfea_lstm.log2
