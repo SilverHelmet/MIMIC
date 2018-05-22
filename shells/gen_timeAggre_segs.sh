30a640fb7a3663831fde200a3c1a7a0cd1b0c792
Haoran Li <lhrshitc@163.com>
2018-05-22 23:30:16 +0800

    auto update

--- shells/gen_timeAggre_segs.sh
+++ shells/gen_timeAggre_segs.sh
@@ -4,11 +4,13 @@
 # dir=death_exper
 # files="death_test_1000.h5 death_train_1000.h5 death_valid_1000.h5"
 # seg_dir=death_exper/segs
-dir=zhu_exper
-files="test.h5 train.h5 valid.h5"
-seg_dir=$dir/segs
+# dir=zhu_exper
+# files="test.h5 train.h5 valid.h5"
+
 dir=lab_exper
 files="labtest_test_1000.h5 labtest_train_1000.h5 labtest_valid_1000.h5"
+
+seg_dir=$dir/segs
 
 for file in $files
 do
