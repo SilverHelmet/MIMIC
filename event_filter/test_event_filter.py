5b85287093458a086ac4b95d88918d6e160353d0
Haoran Li <lhrshitc@163.com>
2018-06-20 05:27:26 +0800

    auto update

--- event_filter/test_event_filter.py
+++ event_filter/test_event_filter.py
@@ -37,10 +37,13 @@ def load_death_timeAggre():
     settings = ["none"] + "settings/fea_gcn.txt settings/catAtt_lstm.txt settings/timeAggre.txt settings/params/gcn_mode-1.txt settings/out_model.txt".split(' ')
     setting = load_argv(settings)
 
+    print 'here'
     model_path = os.path.join(model_dir, 'death_timeAggre_fea_catAtt_gcn.model')
     dataset_path = os.path.join(death_exper_dir, 'death_test_1000.h5')
     seg_path = os.path.join(death_exper_dir, 'segs/death_test_1000_segmode=timeAggre_maxchunk=32.h5')
+    print '1'
     sorted_events = load_event_rank(os.path.join(model_dir, 'event_filter/event_scores_300.txt'))
+    print '2'
     
     Print('load model from [%s]' %model_path)
     model = load_model(model_path, get_custom_objects())
