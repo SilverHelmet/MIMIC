e7d81ca4c33d4651800ca0849827d64f0d65f904
Haoran Li <lhrshitc@163.com>
2018-08-19 21:59:58 +0800

    auto update

--- helstm/task.py
+++ helstm/task.py
@@ -50,11 +50,11 @@ def load_data_all(dataset, time_off):
         ICU_valid_data = load_data(data_path, file_name, time_off) 
     elif dataset == 'icd9':
         data_path = 'icd_exper/'
-        file_name = "icd9_test_1000.h5"
+        file_name = "icd9_test_5000.h5"
         ICU_test_data = load_data(data_path, file_name, time_off)
-        file_name = "icd9_train_1000.h5"
+        file_name = "icd9_train_5000.h5"
         ICU_train_data = load_data(data_path, file_name, time_off)
-        file_name = "icd9_valid_1000.h5"
+        file_name = "icd9_valid_5000.h5"
         ICU_valid_data = load_data(data_path, file_name, time_off)
     elif dataset == 'icd9_rt':
         data_path = 'icd_exper/'
