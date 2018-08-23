d05bd867d2e6383c5fe1b10e8b535f1ea364df23
Haoran Li <lhrshitc@163.com>
2018-08-23 22:44:24 +0800

    auto update

--- test_model.py
+++ test_model.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     args = 'x settings/catAtt_lstm.txt settings/helstm.txt settings/time_feature/time_feature_sum.txt settings/period/period_v14.txt "@num_gate_head=8|model_out=RNNmodels/death_t23.model"'.split(' ')
     setting = load_argv(args)
     setting['event_dim'] = 3418
-    model_path = sys.argv[1]
+    # model_path = sys.argv[1]
     model = define_simple_seg_rnn(setting, True)
     model.load_weights(model_path, by_name=True)
 
