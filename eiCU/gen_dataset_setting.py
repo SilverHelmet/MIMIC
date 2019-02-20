7d00ef9e0115565492fbde1ddd79479023ec4331
Haoran Li <lhrshitc@163.com>
2019-02-20 21:28:05 +0800

    auto update

---
+++ eiCU/gen_dataset_setting.py
@@ -0,0 +1,30 @@
+import os
+import pandas
+from tqdm import tqdm
+from util import eiCU_data_dir, Print
+import glob
+
+def collect_puid(puid_path):
+    if os.path.exists(puid_path):
+        return
+    coded_event_dir = os.path.join(eiCU_data_dir, 'coded_event')
+    puids = set()
+    for filepath in glob.glob(coded_event_dir + '/*csv'):
+        filename = os.path.basename(filepath)
+        table_name = filename[:filename.find('_event_coded.csv')]
+        Print('collect puid from [%s]' %table_name)
+        t = pandas.read_csv(filepath)
+        table_puids = set(t['puid'])
+        puids.union(table_puids)
+    with file(puid_path, 'w') as wf:
+        for puid in sorted(puids):
+            wf.write(str(puid) + '\n')
+
+if __name__ == "__main__":
+    result_dir = os.path.join(eiCU_data_dir, 'result')
+    puid_path = os.path.join(result_dir, 'puids.txt')
+    
+    collect_puid(puid_path)
+
+
+    
