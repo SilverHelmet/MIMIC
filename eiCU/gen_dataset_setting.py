a800b0551ff80fc0f4ef7d87e83e2633a01e899f
Haoran Li <lhrshitc@163.com>
2019-02-20 21:34:59 +0800

    auto update

--- eiCU/gen_dataset_setting.py
+++ eiCU/gen_dataset_setting.py
@@ -9,13 +9,16 @@ def collect_puid(puid_path):
         return
     coded_event_dir = os.path.join(eiCU_data_dir, 'coded_event')
     puids = set()
+    Print('---- collect puid ----')
     for filepath in glob.glob(coded_event_dir + '/*csv'):
         filename = os.path.basename(filepath)
         table_name = filename[:filename.find('_event_coded.csv')]
-        Print('collect puid from [%s]' %table_name)
+        Print('\tcollect puid from [%s]' %table_name)
         t = pandas.read_csv(filepath)
         table_puids = set(t['puid'])
         puids.union(table_puids)
+        break
+    Print('---- write to [%s] ----' %puid_path)
     with file(puid_path, 'w') as wf:
         for puid in sorted(puids):
             wf.write(str(puid) + '\n')
