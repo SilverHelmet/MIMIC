874573a8f7fc8ce2656956620fe5e76bbd155be9
Haoran Li <lhrshitc@163.com>
2019-02-21 09:55:34 +0800

    auto update

--- eiCU/gen_event_list.py
+++ eiCU/gen_event_list.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import glob
+import numpy as np
 from tqdm import tqdm
 from util import eiCU_data_dir, Print, get_nb_lines
 from collections import defaultdict
@@ -17,6 +18,7 @@ class Event:
     def parse_line(line):
         p = line.rstrip().split(',')
         _, offset, puid, num_fea_str, code = p
+        puid = int(puid)
         fea_str_list = num_fea_str.split('$')
         fea_idxs = []
         fea_values = []
@@ -32,10 +34,25 @@ class Event:
     def __str__(self):
         return ",".join(map(str, [self.offset, self.code] + self.fea_idxs  + self.fea_values))
 
-
 def gen_event_list():
-    event_list_map = defaultdict(list)
+    result_dir = os.path.join(eiCU_data_dir, 'result')
+    puid_path = os.path.join(result_dir, 'puids.txt')
+    dataset_dir = os.path.join(eiCU_data_dir, 'dataset')
+    outpath = os.path.join(dataset_dir, 'event_list.txt')
+    if os.path.exists(outpath):
+        os.remove(outpath)
+    puids = map(int, file(puid_path).readlines())
+    ratio = np.arange(0, 1, 0.2)
+    for st in ratio:
+        ed = st + 0.2
+        st_p = int(len(puids) * st)
+        ed_p = int(len(puids) * ed)
+        section = set(puids[st_p:ed_p])
+        Print('---- gen event list of {} ~ {} ----'.format(st, ed))
+        _gen_event_list(section)
 
+def _gen_event_list(puid_set):
+    event_list_map = defaultdict(list)
     event_dir = os.path.join(eiCU_data_dir, 'coded_event')
     for filepath in glob.glob(event_dir + '/*csv'):
         Print('load event from [%s]' %os.path.basename(filepath))
@@ -43,13 +60,15 @@ def gen_event_list():
             if line.startswith(','):
                 continue
             puid, event = Event.parse_line(line)
+            if puid not in puid_set:
+                continue
             event_list_map[puid].append(event)
 
     dataset_dir = os.path.join(eiCU_data_dir, 'dataset')
     if not os.path.exists(dataset_dir):
         os.mkdir(dataset_dir)
     outpath = os.path.join(dataset_dir, 'event_list.txt')
-    wf = file(outpath, 'w')
+    wf = file(outpath, 'a')
     for puid in sorted(event_list_map.keys()):
         event_list = event_list_map[puid]
         event_list.sort(key=lambda x: (x.offset, x.code))
